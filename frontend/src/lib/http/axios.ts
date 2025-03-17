fc8dade15c0199ea75c89da252571819b339a329
Sami MADINI <sami.madini@gmail.com>
2025-03-17 17:07:16 -0400

    chore: add dockerfile for frontend

--- frontend/src/lib/http/axios.ts
+++ frontend/src/lib/http/axios.ts
@@ -1,8 +1,21 @@
-import axios from "axios";
+import axios from "axios"
 
-const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") {
+    if (BASE_URL && BASE_URL.includes("gateway")) {
+      return BASE_URL.replace("http://gateway", "http://localhost")
+    }
+  } else {
+    if (BASE_URL && BASE_URL.includes("localhost")) {
+      return BASE_URL.replace("http://localhost", "http://gateway")
+    }
+  }
+  return BASE_URL
+}
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: getBaseUrl(),
   headers: { "Content-Type": "application/json" },
-});
+})
